15230c360c9483551dff06b9a57fc72038da786c
Tsung-Ying Lee <75181935+tsungyinglee@users.noreply.github.com>
2025-09-24 03:29:31 +0000

    Add initial R script for data science setup with tidyverse and additional packages

---
+++ R-for-Data-Science-2e/Introduction.r
@@ -0,0 +1,12 @@
+install.packages("tidyverse")
+library(tidyverse)
+# tidyverse_update()
+
+install.packages(
+    c(
+        "arrow", "babynames", "curl", "duckdb", "gapminder",
+        "ggrepel", "ggridges", "ggthemes", "hexbin", "janitor", "Lahman",
+        "leaflet", "maps", "nycflights13", "openxlsx", "palmerpenguins",
+        "repurrrsive", "tidymodels", "writexl"
+    )
+)
